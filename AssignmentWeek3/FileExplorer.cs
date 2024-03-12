9132ead768e377ce18389b6db559ea9700cebd60
w3schools-test <test@w3schools.com>
2024-03-12 13:56:12 +0700

    pic update

--- AssignmentWeek3/FileExplorer.cs
+++ AssignmentWeek3/FileExplorer.cs
@@ -159,6 +159,7 @@ namespace SimpleFileExplorer
                         {
                             displayImg dis = new displayImg(path);
                             dis.ShowDialog();
+
                         }
                     }              
                 }
